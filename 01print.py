bff0707415f38ed959ab64052af27bb1d48fb1f8
Catcoderpython <33003204+Catcoderpython@users.noreply.github.com>
2017-10-22 11:36:28 -0400

    Update 01print.py

--- 01print.py
+++ 01print.py
@@ -1,4 +1,8 @@
 #! usr/bin/python
 
-#print can be used to print lines:
+#print can be used to print a number or letter:
+print("A")
+print("1")
+
+#print can be used to print strings:
 print("Hello world")
