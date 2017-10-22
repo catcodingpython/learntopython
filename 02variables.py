300afe4fd0989e72460b730c91db198e4cec1bb3
Catcoderpython <33003204+Catcoderpython@users.noreply.github.com>
2017-10-22 11:35:15 -0400

    Create 02variables.py

---
+++ 02variables.py
@@ -0,0 +1,14 @@
+#! usr/bin/python
+
+#Variables can be used for storing data like letters and numbers:
+
+#assign variables:
+var1 = "Hi"
+num1 = 16
+
+#print variables:
+print(var1)
+print(num1)
+
+#note that variables can be named anything except functions in python like "pass"
+
